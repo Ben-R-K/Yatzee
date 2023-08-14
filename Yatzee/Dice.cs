5b8ee1dedb152c2c8a3c9cba3dfde36b08dcd567
Ben Krüger <benx0071@edu.sde.dk>
2023-08-14 09:58:16 +0200

    Created the Dice and Player Class, too later be uset as objects.

---
+++ Yatzee/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzee
+{
+    public class Dice
+    {
+        public int Id { get; set; }
+        public int Sides { get; set; }
+
+        public Dice(int id, int sides)
+        {
+            Id= id;
+            Sides = sides;
+        }
+    }
+}
---
+++ Yatzee/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzee
+{
+    public class Player
+    {
+        public Dictionary<string, int?> YatzeeCard = new Dictionary<string, int?>();
+
+        public Player(Dictionary<string, int?> yatzeeCard)
+        {
+            YatzeeCard = yatzeeCard;
+        }
+    }
+}
