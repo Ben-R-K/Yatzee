7334afec170f713005738eec53b0c16ba095b20a
Ben Krüger <benx0071@edu.sde.dk>
2023-08-14 12:12:10 +0200

    Added more complex number generator and created Roll method in Dice Class

--- Yatzee/Dice.cs
+++ Yatzee/Dice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace Yatzee
         {
             Id= id;
             Sides = sides;
+        }
+
+        public int Roll()
+        {
+            return RandomNumberGenerator.NumberBetween(0, Sides);
         }
     }
 }
